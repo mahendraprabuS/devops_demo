653178398bbb901169da58d7b35ccd370dab5b39
bhaarn <bharani.dharan1@tcs.com>
2021-10-03 00:33:11 +0530

    Changed for testing

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Hme Page'),
+      home: MyHomePage(title: 'Home Page'),
     );
   }
 }
